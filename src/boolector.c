bitwuzla/bitwuzla

Add API function boolector_get_value.

src/boolector.c
@@ -5252,6 +5252,33 @@ boolector_fun_sort_check(Bzla *bzla,
 
 /*------------------------------------------------------------------------*/
 
+BoolectorNode *
+boolector_get_value(Bzla *bzla, BoolectorNode *node)
+{
+  BzlaNode *res;
+  BzlaNode *exp;
+
+  exp = BZLA_IMPORT_BOOLECTOR_NODE(node);
+  BZLA_ABORT_ARG_NULL(bzla);
+  BZLA_ABORT(
+      bzla->last_sat_result != BZLA_RESULT_SAT || !bzla->valid_assignments,
+      "cannot retrieve model if input formula is not SAT");
+  BZLA_ABORT(!bzla_opt_get(bzla, BZLA_OPT_MODEL_GEN),
+             "model generation has not been enabled");
+  BZLA_ABORT(bzla->quantifiers->count,
+             "models are currently not supported with quantifiers");
+  BZLA_ABORT_ARG_NULL(exp);
+  BZLA_TRAPI_UNFUN(exp);
+  BZLA_ABORT_REFS_NOT_POS(exp);
+  BZLA_ABORT_BZLA_MISMATCH(bzla, exp);
+  res = bzla_model_get_value(bzla, exp);
+  BZLA_TRAPI_RETURN_NODE(res);
+#ifndef NDEBUG
+  BZLA_CHKCLONE_RES_PTR(res, get_value, BZLA_CLONED_EXP(exp));
+#endif
+  return BZLA_EXPORT_BOOLECTOR_NODE(res);
+}
+
 const char *
 boolector_bv_assignment(Bzla *bzla, BoolectorNode *node)
 {

5l1v3r1/boolector

src/boolector.c
@@ -3984,6 +3984,33 @@ boolector_fun_sort_check (Btor *btor,
 
 /*------------------------------------------------------------------------*/
 
+BoolectorNode *
+boolector_get_value (Btor *btor, BoolectorNode *node)
+{
+  BtorNode *res;
+  BtorNode *exp;
+
+  exp = BTOR_IMPORT_BOOLECTOR_NODE (node);
+  BTOR_ABORT_ARG_NULL (btor);
+  BTOR_ABORT (
+      btor->last_sat_result != BTOR_RESULT_SAT || !btor->valid_assignments,
+      "cannot retrieve model if input formula is not SAT");
+  BTOR_ABORT (!btor_opt_get (btor, BTOR_OPT_MODEL_GEN),
+              "model generation has not been enabled");
+  BTOR_ABORT (btor->quantifiers->count,
+              "models are currently not supported with quantifiers");
+  BTOR_ABORT_ARG_NULL (exp);
+  BTOR_TRAPI_UNFUN (exp);
+  BTOR_ABORT_REFS_NOT_POS (exp);
+  BTOR_ABORT_BTOR_MISMATCH (btor, exp);
+  res = btor_model_get_value (btor, exp);
+  BTOR_TRAPI_RETURN_NODE (res);
+#ifndef NDEBUG
+  BTOR_CHKCLONE_RES_PTR (res, get_value, BTOR_CLONED_EXP (exp));
+#endif
+  return BTOR_EXPORT_BOOLECTOR_NODE (res);
+}
+
 const char *
 boolector_bv_assignment (Btor *btor, BoolectorNode *node)
 {